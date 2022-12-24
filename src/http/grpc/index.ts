846f98b73c76e1049bd9541f9f491a05f60e5b8a
Guilherme Gonçalves dos Santos <guilhermegufes@gmail.com>
2022-12-24 17:07:10 -0300

    create deploy pipeline

--- src/http/grpc/index.ts
+++ src/http/grpc/index.ts
@@ -26,7 +26,7 @@ const Product = grpc.loadPackageDefinition(productLoader).ProductService
 
 export default {
   // @ts-ignore
-  user: new User('localhost:50051', grpc.credentials.createInsecure()),
+  user: new User('ms-user.svc.cluster.local:50051', grpc.credentials.createInsecure()),
   // @ts-ignore
-  product: new Product('localhost:50052', grpc.credentials.createInsecure()),
+  product: new Product('ms-catalog.svc.cluster.local:50051', grpc.credentials.createInsecure()),
 };
